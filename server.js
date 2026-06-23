0a057563ac93c2eff63e317453bc699e270fb575
Neox <neoxhere123@gmail.com>
2026-06-23 14:19:24 +0530

    us.js

--- server.js
+++ server.js
@@ -629,7 +629,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
-    console.log("BhatsChat running");
-    console.log("http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
